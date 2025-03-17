09e1728928d7cfb51f387094fe4696ab4663eafb
PedroBataglia <phenriquebataglia@gmail.com>
2025-03-17 19:28:08 -0300

    basic of a shell

---
+++ app/main.php
@@ -0,0 +1,24 @@
+<?php
+error_reporting(E_ALL);
+
+while(true) {
+    fwrite(STDOUT, "$ ");
+
+    $input = fgets(STDIN);
+
+    if ($input == false) {
+        exit(1);
+    }
+
+    $input = trim($input);
+
+    switch($input) {
+        case "exit 0":
+            exit(0);
+        
+        default:
+        fwrite(STDOUT, "$input: command not found\n");
+        break;
+    }
+};
+    
