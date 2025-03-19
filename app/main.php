22d72db33078b11f850df0fffcbc625e1bba4bf0
Pedro-Bataglia <phenriquebataglia@gmail.com>
2025-03-19 10:07:08 -0300

    remove unecessary code

--- app/main.php
+++ app/main.php
@@ -38,23 +38,3 @@ while(true) {
     }
 echo($input . ": command not found" . PHP_EOL);
 }
-while(true) {
-    fwrite(STDOUT, "$ ");
-
-    $input = fgets(STDIN);
-
-    if ($input == false) {
-        exit(1);
-    }
-
-    $input = trim($input);
-
-    switch($input) {
-        case "exit 0":
-            exit(0);
-        
-        default:
-        fwrite(STDOUT, "$input: command not found\n");
-        break;
-    }
-};
